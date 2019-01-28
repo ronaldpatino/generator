4a955a90b10844bce857d024a095de8c9ffd81af
Ronald <ronaldpatino@yahoo.com>
2019-01-28 16:49:01 -0500

    Code cleanup

--- src/main/java/com/sigecloud/Main.java
+++ src/main/java/com/sigecloud/Main.java
@@ -17,35 +17,5 @@ public class Main {
 
         System.out.println(widget);
 
-        /*
-
-        VelocityEngine ve = new VelocityEngine();
-        ve.setProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, "src/main/resources/templates");
-        ve.init();
-
-        Template t = ve.getTemplate("controller.vm");
-        VelocityContext context = new VelocityContext();
-        context.put("className", "Impuesto");
-        context.put("classNameInstance", "impuesto");
-        context.put("daoName", "impuestoDao");
-        context.put("objectName", "io");
-        context.put("sortBy", "nombre");
-        context.put("findBy", "nombre");
-
-        List<Campo> campos = new ArrayList<Campo>();
-
-        campos.add(new Campo("id", "int", null,0, 0, 0, "Id", 0));
-        campos.add(new Campo("nombre", "String", "Debe ingresar en nombre del Impuesto",2, 20, 1, "Nombre del Impuesto", 1));
-        campos.add(new Campo("porcentaje", "BigDecimal", "Debe ingresar en porcentaje del Impuesto",2, 10, 1, "Porcentaje del Impuesto", 1));
-        campos.add(new Campo("tipo", "String", "Seleccine el tipo de Impuesto",2, 10, 1, "Tipo de impuesto", 1));
-        campos.add(new Campo("codigoSri", "String", "Seleccine el tipo de Impuesto",2, 10, 1, "Codigo SRI", 1));
-        context.put("fields", campos);
-
-
-        StringWriter writer = new StringWriter();
-        t.merge( context, writer );
-        System.out.println( writer.toString() );
-
-        */
     }
 }
